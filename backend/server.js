d8b23cc6a634899ca621905ac4f7c805a336ea02
katochkeshav63-wq <katochkeshav63@gmail.com>
2026-04-07 12:43:00 +0530

    change in server

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,23 @@ const app = express();
 //     credentials: true,
 //   })
 // );
-app.use(cors())
+const allowedOrigins = [
+  "https://e-com-website-5.onrender.com/",
+  "https://e-com-website-4.onrender.com/"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 // ✅ Handle preflight requests
 // app.options("*", cors());
 
