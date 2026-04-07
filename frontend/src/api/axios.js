a8940b9b954166b913b15425b67fa77eb0e7b7f1
katochkeshav63-wq <katochkeshav63@gmail.com>
2026-04-07 12:05:26 +0530

    axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: "https://e-com-website-3.onrender.com/api"
+     baseURL: apiUrl
 })
 
 api.interceptors.request.use((config) => {
